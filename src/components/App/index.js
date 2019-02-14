bd5d592c51a089b8e859abdaaa2dc7ebf0b20365
GUZMER9 <eric.guzman@verizon.com>
2019-02-14 16:08:59 -0400

    Material UI set

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,9 @@ import {
   BrowserRouter as Router,
   Route,
  } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -15,23 +18,41 @@ import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
 
-const App = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+});
+
+const App = (props) => {
+  // const { classes } = props;
+
   return(
     <Router>
       <Fragment>
         <Navigation />
 
-        <hr />
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            {/* <Paper className={classes.paper}> */}
+              <Route exact path={ROUTES.LANDING} component={LandingPage} />
+              <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+              <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+              <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+              <Route path={ROUTES.HOME} component={HomePage} />
+              <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+              <Route path={ROUTES.ADMIN} component={AdminPage} />
+            {/* </Paper> */}
+          </Grid>
+        </Grid>
       </Fragment>
     </Router>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,31 +1,71 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import pink from '@material-ui/core/colors/pink';
 
+import '../styles/common.css';
 import * as ROUTES from '../../constants/routes'; 
 
-const Navigation = () => {
+const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue,
+    secondary: pink,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
+
+const styles = (theme) => ({
+  root: {
+    with: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
+
+const Navigation = (props) => {
+  const { classes } = props;
 
   return(
-    <Fragment>
-      <ul>
-        <li>
-          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-      </ul>
-    </Fragment>
+    <div className={classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <Typography className={classes.title} variant="h6" color="inherit" noWrap> */}
+              React Firebase Authentication
+            {/* </Typography> */}
+            <div className={classes.grow} />
+            <Button color="secondary" className={classes.button}>
+              <Link to={ROUTES.SIGN_IN} className="rlink">Sign In</Link>
+            </Button> 
+            <Button color="secondary" className={classes.button}>
+              <Link to={ROUTES.LANDING} className="rlink">Landing</Link>
+            </Button>
+            <Button color="secondary" className={classes.button}>
+              <Link to={ROUTES.HOME} className="rlink">Home</Link>
+            </Button> 
+            <Button color="secondary" className={classes.button}>
+              <Link to={ROUTES.ACCOUNT} className="rlink">Account</Link>
+            </Button>
+            <Button color="secondary" className={classes.button}>
+              <Link to={ROUTES.ADMIN} className="rlink">Admin</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </MuiThemeProvider>
+    </div>
   );
 }
 
-export default Navigation;
+export default withStyles(styles(theme))(Navigation);
