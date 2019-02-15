25552707a3a443f9521d218aed89349a2a6115ad
GUZMER9 <eric.guzman@verizon.com>
2019-02-15 11:27:49 -0400

    Firebase class required methods

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,15 +1,7 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
-const prodConfig = {
-  apiKey: process.env.REACT_APP_PROD_API_KEY,
-  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-};
-
-const devConfig = {
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -18,13 +10,30 @@ const devConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
-const config =
-  process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
-
 class Firebase {
   constructor() {
-    app.initializeApp(config)
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // Auth API 
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password) => {
+    this.auth.currentUser.updatePassword(password);
   }
 }
+
 
 export default Firebase;
