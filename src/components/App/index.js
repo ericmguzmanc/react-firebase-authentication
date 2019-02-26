d9230b4965e40a3482a18269a327e2cd0299715e
GUZMER9 <eric.guzman@verizon.com>
2019-02-26 10:06:18 -0400

    Initial session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { 
   BrowserRouter as Router,
   Route,
@@ -14,26 +14,48 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
-const App = (props) => {
+// const App = (props) => {
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  return(
-    <Router>
-      <Fragment>
-        <Navigation />
+    this.state = {
+      authUser: null,
+    }
+  }
 
-        <div className="container-fluid">
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-        </div>
-      </Fragment>
-    </Router>
-  );
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null })
+    })
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return(
+      <Router>
+        <Fragment>
+          <Navigation authUser={this.state.authUser} />
+          <div className="container-fluid">
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route path={ROUTES.ADMIN} component={AdminPage} />
+          </div>
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -13,10 +13,46 @@ import '../styles/common.css';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes'; 
 
-const Navigation = (props) => {
+const Navigation = ({ authUser }) => (
+  <Fragment>{ authUser ? <NavigationAuth /> : <NavigationNonAuth />}</Fragment>
+);
+
+
+const NavigationAuth = () => (
+  <Fragment>
+    <Navbar color="dark" light expand="md" className="main-navbar">
+      <NavbarBrand href="/">React Firebase Authentication</NavbarBrand>
+      <NavbarToggler />
+      <Collapse navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <div className="navItem-li">
+              <Link to={ROUTES.LANDING} className="rlink">Landing</Link>
+            </div>
+          </NavItem>
+          <NavItem>
+            <div className="navItem-li">
+              <Link to={ROUTES.HOME} className="rlink">Home</Link>
+            </div>
+          </NavItem>
+          <NavItem>
+            <div className="navItem-li">
+              <Link to={ROUTES.ACCOUNT} className="rlink">Account</Link>
+            </div>
+          </NavItem>
+          <NavItem>
+            <SignOutButton />
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  </Fragment>
+);
+
+const NavigationNonAuth = () => {
 
   return(
-    <div>
+    <Fragment>
       <Navbar color="dark" light expand="md" className="main-navbar">
         <NavbarBrand href="/">React Firebase Authentication</NavbarBrand>
         <NavbarToggler />
@@ -24,36 +60,18 @@ const Navigation = (props) => {
           <Nav className="ml-auto" navbar>
             <NavItem>
               <div className="navItem-li">
-                <Link to={ROUTES.SIGN_IN} className="rlink">Sign In</Link>
-              </div>
-            </NavItem>
-            <NavItem>
-              <div className="navItem-li">
                 <Link to={ROUTES.LANDING} className="rlink">Landing</Link>
               </div>
             </NavItem>
             <NavItem>
               <div className="navItem-li">
-                <Link to={ROUTES.HOME} className="rlink">Home</Link>
+                <Link to={ROUTES.SIGN_IN} className="rlink">Sign In</Link>
               </div>
-            </NavItem>
-            <NavItem>
-              <div className="navItem-li">
-                <Link to={ROUTES.ACCOUNT} className="rlink">Account</Link>
-              </div>
-            </NavItem>
-            <NavItem>
-              <div className="navItem-li">
-                <Link to={ROUTES.ADMIN} className="rlink">Admin</Link>
-              </div>
-            </NavItem>
-            <NavItem>
-              <SignOutButton />
             </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
-    </div>
+    </Fragment>
   );
 }
 
